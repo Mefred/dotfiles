4e57dbf24dd553a40a5c7677e1b0cb3a468ba7c2
Mefred <freds-inbox@outlook.com>
2026-05-28 21:19:34 -0700

    main things

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+require("config.lazy")
+
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("catppuccin")
